f91d75db1eefceb0d7c393c1e923725f1d47f1ad
leonel <leo87llanca@gmail.com>
2023-10-30 22:45:25 -0300

    borrado de proyecto java

--- Code/src/tp1/Handler.java
+++
@@ -1,8 +0,0 @@
-package tp1;
-
-public interface Handler {
-    public void setNext(Handler h);
-
-    public void handler(Peticion peticion);
-
-}
--- Code/src/tp1/webBootstrap/conexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    $conexion= mysql_connect('localhost','leonel','1234','')
-
-
-?>
